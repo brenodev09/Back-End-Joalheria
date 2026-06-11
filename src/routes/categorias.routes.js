013cf9368fe503778bad4716921422465aff3ad9
brenodev09 <brenolufy10@gmail.com>
2026-06-11 15:46:48 -0300

    método post categoria

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -20,4 +20,36 @@ router.get("/", async (req,res) =>{
     }
 })
 
+router.post("/", async (req, res) =>{
+
+    try{
+        const {nome, descricao, imagem, ativo} = req.body
+
+        if(!nome || !descricao) {
+            return res.status(400).json({
+                erro:"Nome e descrição são obrigatórios"
+            })
+        }
+
+        const sql = `insert into categorias (nome, descricao, imagem, ativo)
+            values(?,?,?,?) `
+
+        const resultado = await db.run(sql, [
+            nome, descricao || null, imagem, ativo
+        ])
+
+        const categoriaCriada =  await db.get( `select id, nome, descricao, imagem, ativo, criado_em, atualizado_em 
+            from itens where id = ?`, [resultado.lastID]),
+            
+          return res.status(201).json(categoriaCriada)  
+    } catch (error){
+        console.error(error)
+
+        return res.status(500).json({
+            erro:"Erro ao criar categoria, por favor tente novamente! "
+        })
+    }
+
+} )
+
 export default router
