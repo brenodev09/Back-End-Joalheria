4f36c2176a7084eae5562317b2c2fced23ed7e19
brenodev09 <brenolufy10@gmail.com>
2026-06-19 13:50:29 -0300

    Método de deletar categoria

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -21,6 +21,7 @@ router.get("/", async (req, res) => {
     }
 })
 
+// método de adicionar uma categoria
 router.post("/", upload.single("imagem"), async (req, res) => {
 
     try {
@@ -63,5 +64,34 @@ router.post("/", upload.single("imagem"), async (req, res) => {
     }
 
 })
+
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const sql = `delete from categorias where id = ?`
+
+        const [resultadoDelete] = await db.query(sql, [id])
+
+        if (resultadoDelete.affectedRows === 0) {
+            return res.status(404).json({
+                erro: "Item não encontrado.",
+            });
+        }
+
+        return res.status(200).json({
+            mensagem: "Categoria excluida com sucesso"
+        })
+    } catch (error) {
+        console.error(error)
+
+        return res.status(500).json({
+            erro: "Erro ao deleter o item! Tente novamente."
+        })
+    }
+})
+
 
 export default router
